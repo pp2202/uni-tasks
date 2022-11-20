f06bea415aee094aed86186454b8ccbaefb7f0f3
pp2202 <77071897+pp2202@users.noreply.github.com>
2022-11-20 21:14:30 +0100

    main function
    includes everything needed to extract video with blue and red hues

---
+++ color_mask/color_extraction.py
@@ -0,0 +1,56 @@
+import cv2 as cv
+import numpy as np
+
+def color_treshhold(a):
+
+    
+    cap = cv.VideoCapture(a)
+
+    frame_width = int(cap.get(3)*0.25)
+    frame_height = int(cap.get(4)*0.25)
+
+    dims = (frame_width,frame_height)
+
+    out = cv.VideoWriter('output.avi',cv.VideoWriter_fourcc('M','J','P','G'), 50, (frame_width,frame_height))
+
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+
+    while(True):
+        ret, frame = cap.read()
+
+        if ret == True:
+
+            frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+            th_frame = frame.copy()
+            lower_red_mask = np.array([0,120,30])
+            
+            upper_red_mask = np.array([4,255,255])
+            
+            lower_blue_mask = np.array([110,200,30])
+            
+            upper_blue_mask = np.array([120,255,255])
+            
+            red_mask = cv.inRange(frame, lower_red_mask, upper_red_mask)
+            
+            blue_mask = cv.inRange(frame, lower_blue_mask, upper_blue_mask)
+            
+            th_frame = cv.bitwise_and(th_frame, th_frame, mask=blue_mask|red_mask)
+
+            rgb_frame = cv.cvtColor(th_frame, cv.COLOR_HSV2BGR)
+
+            resized_frame = cv.resize(rgb_frame, dims, interpolation=cv.INTER_CUBIC)
+
+            out.write(resized_frame)
+
+        else:
+            break
+
+    cap.release()
+    out.release()
+
+
+
+color_treshhold("one round kam4.mp4")
+                
+
