3f7f2bef57b03892a381792ad22a21d054a600c3
pp2202 <77071897+pp2202@users.noreply.github.com>
2022-12-19 21:13:55 +0100

    0.1.1c

--- image-classification/code_classes/model.py
+++ image-classification/code_classes/model.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 class ModelCreate():
 
-
     def create():
+        
         model = tf.keras.Sequential()
         model.add(tf.keras.layers.Input(shape = (150,150,3)))
         model.add(tf.keras.layers.Conv2D(filters = 32, strides = (2,2), padding='same', activation='relu'))
@@ -27,7 +27,7 @@ class ModelCreate():
         model.add(tf.keras.layers.SeparableConv2D(filters = 128, kernel_size = (4,4), strides = (1,1), padding = 'same', activation = 'relu'))
         model.add(tf.keras.layers.MaxPooling2D())
         model.add(tf.keras.layers.Conv2D(filters = 128, kernel_size = (4,4), strides = (1,1), padding = 'same', activation = 'relu', kernel_regularizer ='l2'))
-        model.add(tf.keras.layers.BatchNormalizatio(naxis = 1))
+        model.add(tf.keras.layers.BatchNormalizatio(axis = 1))
         model.add(tf.keras.layers.Dropout(0.4))
         model.add(tf.keras.layers.Dense(6, activation = 'softmax'))
 
