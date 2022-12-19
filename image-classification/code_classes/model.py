95692d3e75a1f30e19ccf360a7d76274019ffab6
pp2202 <77071897+pp2202@users.noreply.github.com>
2022-12-19 13:54:40 +0100

    0.1.1a

--- image-classification/code_classes/model.py
+++ image-classification/code_classes/model.py
@@ -1,2 +1,15 @@
-class Model():
-    print('a')
+import tensorflow as tf
+
+class ModelCreate():
+
+
+    def create():
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Input(shape = (150,150,3)))
+        model.add(tf.keras.layers.SeparableConv2D(filters = 32, kernel_size = (3,3), padding = 'same', activation = 'relu'))
+        model.add(tf.keras.layers.MaxPooling2D())
+        model.add(tf.keras.layers.SeparableConv2D(filters = 32, kernel_size = (3,3), padding = 'same', activation = 'relu'))
+        model.add(tf.keras.layers.MaxPooling2D())
+        model.add(tf.keras.layers.SeparableConv2D(filters = 16, kernel_size = (3,3), padding = 'same', activation = 'relu'))
+        model.add(tf.keras.layers.MaxPooling2D())
+        model.add(tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'))
